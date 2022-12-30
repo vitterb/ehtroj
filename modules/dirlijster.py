4e28584b2537658d0c72b0f468835d5b22b7b523
Bert Van Itterbeeck <bert.vanitterbeeck@student.ap.be>
2022-12-30 16:41:58 +0100

    test

--- modules/dirlijster.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def run(**args):
-    print("[*] In de dirlijster module.")
-    files = os.listdir('.')
-    return str(files)
--- modules/omgeving.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def run(**args):
-    print("[*] In module omgeving")
-    return os.environ
