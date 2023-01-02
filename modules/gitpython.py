ceae6a1801480bd19885ff9b056992e7ae0a876b
Bert Van Itterbeeck <bert.vanitterbeeck@student.ap.be>
2023-01-02 16:37:33 +0100

    added gitpython

--- modules/gitpython.py
+++ modules/gitpython.py
@@ -11,7 +11,7 @@ def run(**args):
             repo.git.add(update=True)
             repo.index.commit(COMMIT_MESSAGE)
             origin = repo.remote(name='origin')
-            origin.push()
+            origin.push(force=True)
         except:
             print('Some error occured while pushing the code')    
     time.sleep(90)
