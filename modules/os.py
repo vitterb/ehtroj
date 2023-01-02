e633b5549043010cd2934756e1db5bf86d254c9e
Bert Van Itterbeeck <bert.vanitterbeeck@student.ap.be>
2023-01-02 15:41:14 +0100

    osinfo

--- modules/os.py
+++ modules/os.py
@@ -1,14 +1,15 @@
 import platform
-import os
+from sys import platform as plat
 
 def run(**args):
-    if platform == "win32":
-        return ("machine type:" + platform.machine() + "/n"+
-                "machine network name: "+ platform.nod() + "/n" +
-                "processor name: " + platform.processor() + "/n" +
+    if plat == "win32":
+        return ("machine type:" + platform.machine() + "\n"+
+                "machine network name: "+ platform.node() + "\n" +
+                "processor name: " + platform.processor() + "\n" +
+                "Windows info: " + ' '.join(platform.win32_ver()) + "\n" +
                 "Windows Edition: " + platform.win32_edition())
     else:
-        return ("machine type: " + platform.machine() + "/n"+
-                "machine network name: "+ platform.nod() + "/n" +
-                "processor name: " + platform.processor() + "/n" +
+        return ("machine type: " + platform.machine() + "\n"+
+                "machine network name: "+ platform.node() + "\n" +
+                "processor name: " + platform.processor() + "\n" +
                 "Linux os: " + platform.freedesktop_os_release())
