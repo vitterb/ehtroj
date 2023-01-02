f22fb12dd7b8b4a2bc8a95e1ad5530dc68af4bdc
Bert Van Itterbeeck <bert.vanitterbeeck@student.ap.be>
2023-01-02 16:36:22 +0100

    added gitpython

---
+++ modules/gitpython.py
@@ -0,0 +1,18 @@
+from git import Repo
+import time
+
+PATH_OF_GIT_REPO = r'C:\schoolwerk\ehtroj\.git'  # make sure .git folder is properly configured
+COMMIT_MESSAGE = 'comment from python script'
+
+def run(**args):
+    def git_push():
+        try:
+            repo = Repo(PATH_OF_GIT_REPO)
+            repo.git.add(update=True)
+            repo.index.commit(COMMIT_MESSAGE)
+            origin = repo.remote(name='origin')
+            origin.push()
+        except:
+            print('Some error occured while pushing the code')    
+    time.sleep(90)
+    git_push()
