ead20348102645458ccd308385934c7c76050433
Bert Van Itterbeeck <bert.vanitterbeeck@student.ap.be>
2023-01-02 15:47:03 +0100

    only win

--- modules/os.py
+++ modules/os.py
@@ -1,8 +1,16 @@
 import platform
+from sys import platform as plat
 
 def run(**args):
-    return  ("machine type:" + platform.machine() + "\n"+
-            "machine network name: "+ platform.node() + "\n" +
-            "processor name: " + platform.processor() + "\n" +
-            "Windows info: " + ' '.join(platform.win32_ver()) + "\n" +
-            "Windows Edition: " + platform.win32_edition())
+
+    if plat == "win32":
+        return ("machine type:" + platform.machine() + "\n"+
+                "machine network name: "+ platform.node() + "\n" +
+                "processor name: " + platform.processor() + "\n" +
+                "Windows info: " + ' '.join(platform.win32_ver()) + "\n" +
+                "Windows Edition: " + platform.win32_edition())
+    else:
+        return  ("machine type: " + platform.machine() + "\n"+
+                "machine network name: "+ platform.node() + "\n" +
+                "processor name: " + platform.processor() + "\n" +
+                "Linux os: " + platform.freedesktop_os_release())
