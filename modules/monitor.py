efd57dc92f9e9be657757adb59b254a9734413a3
Bert Van Itterbeeck <bert.vanitterbeeck@student.ap.be>
2023-01-02 15:01:37 +0100

    monitor

--- modules/monitor.py
+++ modules/monitor.py
@@ -5,19 +5,31 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from watchdog.events import PatternMatchingEventHandler
 
+
 def run(**args):
+
     patterns = ["*"]
     ignore_patterns = None
     ignore_directories = False
     case_sensitive = True
-    keyEvent = False
     my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
 
     def on_created(event):
-        print(f"hey, {event.src_path} has been created!")
-        keyEvent = True
+        return(f"hey, {event.src_path} has been created!")
+
+    def on_deleted(event):
+        return(f"what the f**k! Someone deleted {event.src_path}!")
+
+    def on_modified(event):
+        return(f"hey buddy, {event.src_path} has been modified")
+
+    def on_moved(event):
+        return(f"ok ok ok, someone moved {event.src_path} to {event.dest_path}")
 
     my_event_handler.on_created = on_created
+    my_event_handler.on_deleted = on_deleted
+    my_event_handler.on_modified = on_modified
+    my_event_handler.on_moved = on_moved
 
     path = "."
     go_recursively = True
@@ -28,10 +40,6 @@ def run(**args):
     try:
         while True:
             time.sleep(1)
-            if keyEvent == True:
-                path = os.getcwd() + '/data'
-                for folder_path, folders, files in os.walk(path):
-                    print(files)
     except KeyboardInterrupt:
         my_observer.stop()
         my_observer.join()
